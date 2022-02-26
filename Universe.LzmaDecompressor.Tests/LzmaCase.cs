c5f34603839982c93d61a2b5df64830ca40b8ee7
devizer <devizer@github.users>
2022-02-27 01:39:45 +0200

    tests

--- Universe.LzmaDecompressor.Tests/LzmaCase.cs
+++ Universe.LzmaDecompressor.Tests/LzmaCase.cs
@@ -1,30 +1,65 @@
 namespace Universe.LzmaDecompressor.Tests
 {
+    using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
+    using System.Linq;
     using NUnit.Framework;
 
     public class LzmaCases
     {
-        public static LzmaCase[] GetCases()
+        public static LzmaCase[] GetCases(char?[] chars, int[] sizes, int[] levels)
         {
             var dir = new DirectoryInfo("LZMA-Test-Temp").FullName;
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
             List<LzmaCase> ret = new List<LzmaCase>();
-            foreach (char? ch in new char?[] {'Z', null})
-                foreach(int size in new [] { 1, 100, 1000, 10000})
+            foreach (int level in levels)
+            foreach (char? ch in chars)
+            foreach (int size in sizes)
+            {
+                var lzmaCase = new LzmaCase()
                 {
-                    var lzmaCase = new LzmaCase()
-                    {
-                        Size = size,
-                        Content = ch,
-                    };
-                    lzmaCase.PlainFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.plain");
-                    lzmaCase.CompressedFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.lzma");
-                    lzmaCase.ActualFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.actual");
-                    ret.Add(lzmaCase);
+                    Size = size,
+                    Content = ch,
+                    Level = level,
+                };
+                lzmaCase.PlainFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.txt");
+                lzmaCase.CompressedFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.txt.lzma");
+                lzmaCase.ActualFile = Path.Combine(dir, $"{lzmaCase.GetFileName()}.actual");
+                ret.Add(lzmaCase);
+            }
+
+            Random rnd = new Random(42);
+            foreach (var lzmaCase in ret)
+            {
+                byte[] content;
+                if (lzmaCase.Content.HasValue)
+                    content = Enumerable.Repeat((byte) lzmaCase.Content.Value, lzmaCase.Size).ToArray();
+                else
+                    content = Enumerable.Repeat(42, lzmaCase.Size).Select(x => (byte)rnd.Next(0, 255)).ToArray();
+
+                using (FileStream fs = new FileStream(lzmaCase.PlainFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    fs.Write(content, 0, content.Length);
                 }
 
+                Console.WriteLine($"Preparing {lzmaCase}");
+                var exe = (CrossInfo.ThePlatform == CrossInfo.Platform.Windows) ? "Binaries\\xz.exe" : "xz";
+                ProcessStartInfo si = new ProcessStartInfo(exe, $"-z -{lzmaCase.Level} -k \"{lzmaCase.PlainFile}\"");
+                using (Process p = Process.Start(si))
+                {
+                    p.WaitForExit();
+                }
+            }
+
             return ret.ToArray();
+        }
+
+        public static LzmaCase[] GetCases()
+        {
+            var levels = Enumerable.Repeat(42, 9).Select((x, index) => index + 1).ToArray();
+            return GetCases(new char?[] {'Z', null}, new[] {1, 100, 1000, 10000}, levels);
         }
     }
     public class LzmaCase
@@ -33,6 +68,7 @@ namespace Universe.LzmaDecompressor.Tests
         public string PlainFile;
         public string ActualFile;
 
+        public int Level;
         public int Size;
         public char? Content; // null - random
 
@@ -42,7 +78,7 @@ namespace Universe.LzmaDecompressor.Tests
         }
         public string GetFileName()
         {
-            return $"{Size:n0} bytes, {(Content == null ? "random" : $"char {Content}")}";
+            return $"{Size:n0} bytes, lvl={Level}, {(Content == null ? "random" : $"char {Content}")}";
         }
     }
 }
