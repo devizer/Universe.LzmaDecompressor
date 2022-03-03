06dd2984400aac71a4ace0b618684e54493de9bf
devizer <devizer@github.users>
2022-03-03 06:37:01 +0200

    get rid of BitEncoder

--- Universe.LzmaDecompressor/LzmaDecompressorImplementation/RangeCoderBit.cs
+++ Universe.LzmaDecompressor/LzmaDecompressorImplementation/RangeCoderBit.cs
@@ -1,58 +1,5 @@
 namespace Universe.LzmaDecompressionImplementation.SevenZip.Compression.RangeCoder
 {
-	internal struct BitEncoder
-	{
-		public const int kNumBitModelTotalBits = 11;
-		public const uint kBitModelTotal = 1 << kNumBitModelTotalBits;
-		private const int kNumMoveBits = 5;
-		private const int kNumMoveReducingBits = 2;
-		public const int kNumBitPriceShiftBits = 6;
-
-		private uint Prob;
-
-		public void Init()
-		{
-			Prob = kBitModelTotal >> 1;
-		}
-
-		public void UpdateModel(uint symbol)
-		{
-			if (symbol == 0)
-				Prob += (kBitModelTotal - Prob) >> kNumMoveBits;
-			else
-				Prob -= Prob >> kNumMoveBits;
-		}
-
-		private static readonly uint[] ProbPrices = new uint[kBitModelTotal >> kNumMoveReducingBits];
-
-		static BitEncoder()
-		{
-			const int kNumBits = kNumBitModelTotalBits - kNumMoveReducingBits;
-			for (int i = kNumBits - 1; i >= 0; i--)
-			{
-				uint start = (uint) 1 << (kNumBits - i - 1);
-				uint end = (uint) 1 << (kNumBits - i);
-				for (uint j = start; j < end; j++)
-					ProbPrices[j] = ((uint) i << kNumBitPriceShiftBits) +
-					                (((end - j) << kNumBitPriceShiftBits) >> (kNumBits - i - 1));
-			}
-		}
-
-		public uint GetPrice(uint symbol)
-		{
-			return ProbPrices[(((Prob - symbol) ^ -(int) symbol) & (kBitModelTotal - 1)) >> kNumMoveReducingBits];
-		}
-
-		public uint GetPrice0()
-		{
-			return ProbPrices[Prob >> kNumMoveReducingBits];
-		}
-
-		public uint GetPrice1()
-		{
-			return ProbPrices[(kBitModelTotal - Prob) >> kNumMoveReducingBits];
-		}
-	}
 
 	internal struct BitDecoder
 	{
